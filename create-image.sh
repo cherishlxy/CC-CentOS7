ac040b2a1a62308a3d1f66f6c06e18dbf1262eb9
Pierre Riteau <priteau@uchicago.edu>
2016-10-09 19:38:51 -0500

    Add Heat contextualization elements

--- create-image.sh
+++ create-image.sh
@@ -4,9 +4,16 @@
 # * via Yum: git python-pip PyYAML qemu-img xfsprogs xz
 # * via Pip: diskimage-builder
 
-
 IMAGE_NAME="CC-CentOS7"
 BASE_IMAGE="CentOS-7-x86_64-GenericCloud-1608.qcow2"
+
+# Clone the required repositories for Heat contextualization elements
+if [ ! -d tripleo-image-elements ]; then
+  git clone https://git.openstack.org/openstack/tripleo-image-elements.git
+fi
+if [ ! -d heat-templates ]; then
+  git clone https://git.openstack.org/openstack/heat-templates.git
+fi
 
 if [ "$BASE_IMAGE" == "" ]; then
   echo "could not identify the last centos qcow2 image from http://cloud.centos.org/centos/7/images/sha256sum.txt"
@@ -33,10 +40,14 @@ fi
 
 xz --decompress --keep $BASE_IMAGE_XZ
 
+# Forces diskimage-builder to install software using package rather than source
+# See http://docs.openstack.org/developer/diskimage-builder/developer/install_types.html
+export DIB_DEFAULT_INSTALLTYPE='package'
 export DIB_LOCAL_IMAGE=`pwd`/$BASE_IMAGE
-export ELEMENTS_PATH=`pwd`/elements
-export FS_TYPE=xfs
-export LIBGUESTFS_BACKEND=direct
+# Required by diskimage-builder to discover element collections
+export ELEMENTS_PATH='elements:tripleo-image-elements/elements:heat-templates/hot/software-config/elements'
+export FS_TYPE='xfs'
+export LIBGUESTFS_BACKEND='direct'
 
 OUTPUT_FILE="$1"
 if [ "$OUTPUT_FILE" == "" ]; then
@@ -45,18 +56,27 @@ if [ "$OUTPUT_FILE" == "" ]; then
   OUTPUT_FILE="$TMPDIR/common/$IMAGE_NAME.qcow2"
 fi
 
-
 ELEMENTS="vm"
 if [ "$FORCE_PARTITION_IMAGE" = true ]; then
   ELEMENTS="baremetal"
 fi
+
+# Install and configure the os-collect-config agent to poll the metadata
+# server (heat service or zaqar message queue and so on) for configuration
+# changes to execute
+export AGENT_ELEMENTS="os-collect-config os-refresh-config os-apply-config"
+
+# heat-config installs an os-refresh-config script which will invoke the
+# appropriate hook to perform configuration. The element heat-config-script
+# installs a hook to perform configuration with shell scripts
+export DEPLOYMENT_BASE_ELEMENTS="heat-config heat-config-script"
 
 if [ -f "$OUTPUT_FILE" ]; then
   echo "removing existing $OUTPUT_FILE"
   rm -f "$OUTPUT_FILE"
 fi
 
-/bin/disk-image-create chameleon-common $ELEMENTS -o $OUTPUT_FILE
+/bin/disk-image-create chameleon-common $ELEMENTS $AGENT_ELEMENTS $DEPLOYMENT_BASE_ELEMENTS -o $OUTPUT_FILE
 
 if [ -f "$OUTPUT_FILE.qcow2" ]; then
   mv $OUTPUT_FILE.qcow2 $OUTPUT_FILE
